68eb4592ee1edd59828a225c35d990c2b8bd14f7
24LP6392 24LP6392 <24LP6392@G303-CC0155.local>
2025-03-27 09:11:26 +0800

    asd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@ import { Button } from "@/components/ui/button";
 import Header from "./componant/Header";
 import { ChevronLeft } from "lucide-react";
 
+
 export default function Home() {
   return (
     <div>
+    <div>
       <Header/>
+   
+    </div>
 
     </div>
   );
